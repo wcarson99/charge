df84ad3b9d7ee8cb1130d5fe298508480375ef10
wcarson99 <96308555+wcarson99@users.noreply.github.com>
2024-02-02 04:06:36 -0500

    Items can be added to the board

--- game.js
+++ game.js
@@ -14,7 +14,8 @@ const boardCenterY = squareY*boardRows/2
 const numItems = 6;
 
 let timeText;
-
+let board;
+let items;
 
 const square_types = {
     '.': 'square_empty',
@@ -49,8 +50,8 @@ const levels = [
                 attack: 2,
                 shield: 2,
                 units:
-                    [{ c: 0, r: 0, typ: 'soldier' },
-                     { c: 3, r: 0, typ: 'soldier' }
+                    [{ c: 0, r: 0, typ: 'unit_soldier' },
+                     { c: 3, r: 0, typ: 'unit_soldier' }
                     ],
                 rowChange: 1
             },
@@ -86,33 +87,60 @@ class Player
 
     }
 }
-class Square
-{
-    constructor(board, index, type, c, r)
-    {
-        this.board = board;
-        this.index = index;
-        this.c = c;
-        this.r = r;
-        this.contents = [];
-        let image = this.board.scene.add.image(c*squareX,r*squareY,type)
-        image.setDisplaySize(squareX,squareY)
-        board.container.add(image)
-    }
-}
 
 class Item
 {   
-    constructor(board, index, type, column)
+    constructor(items, index, typ, column)
     {
-        this.board = board;
-        this.index = index;
+        this.items = items
+        this.index = index
+        this.typ = typ
         this.column = column;
-        let image = this.board.scene.add.image(column*squareX,0,type)
+        let image = this.items.scene.add.image(column*squareX,0,typ)
+        image.setData('obj', this)
+        image.setData('newSquare',undefined)
         image.setDisplaySize(squareX,squareY)
+        image.setSize(squareX,squareY)
         image.setInteractive({ draggable: true})
-        board.items.add(image)
+        items.container.add(image)
+        items.group.add(image)
     }
+}
+
+class Items
+{
+    constructor(scene, levelNum)
+    {
+        this.scene = scene
+        this.level = levels[levelNum]
+
+        this.container = scene.add.container(100,820)
+        this.container.setSize(numItems*squareX,squareY)
+        this.group = scene.physics.add.group(
+            {
+            
+            }
+        )
+        this.addItems();
+    }
+    remove(item)
+    {
+        console.log('Removing')
+        console.log(item)
+        this.group.remove(item)
+    }
+
+    addItems()
+    {   
+        let i = 0;
+        this.itemData = []
+        for (let c = 0; c<numItems; c++)
+        {
+            this.itemData[c] = new Item(this, i, 'unit_soldier',c,0)
+            i++
+        }
+    }
+
 }
 
 class Unit
@@ -153,6 +181,27 @@ class Unit
     }
 }
 
+class Square
+{
+    constructor(board, index, type, c, r)
+    {
+        this.board = board;
+        this.index = index;
+        this.column = c;
+        this.row = r;
+        //this.contents = [];
+        let image = board.scene.add.image(c*squareX,r*squareY,type)
+        image.setDisplaySize(squareX,squareY)
+        image.setSize(squareX,squareY)
+        image.setData('row',r)
+        image.setData('column',c)
+        image.setData('contents',[])
+        board.container.add(image)
+        this.image = image
+        board.group.add(image) 
+    }
+}
+
 class Board
 /*
 TODO: Should map be a separate object?
@@ -164,18 +213,18 @@ TODO: Should map be a separate object?
         this.rows = rows;
         this.columns = columns
         this.level = levels[levelNum]
+        this.unitIndex = 0
         console.log(this.level)
         let x = squareX*columns
         let y = squareY*rows
         
+        this.group = scene.physics.add.group()
         this.container = scene.add.container(100,120)
         this.container.setSize(columns*squareX, rows*squareY)
         this.createMap()
-        this.addUnits()
+        this.addStartingUnits()
 
-        this.items = scene.add.container(100,820)
-        this.items.setSize(numItems*squareX,squareY)
-        this.createItems();
+        //const physicsContainer = scene.matter.add.gameObject(this.container);
 
         this.computer = new Player(this, 100,60, this.level.players.computer)
         this.human = new Player(this, 100,700, this.level.players.human)
@@ -206,56 +255,66 @@ TODO: Should map be a separate object?
             for (let square of r)
             {
                 // Pop contents and add them to the appropriate square
-                let contents = square.contents
+                console.log(square.image)
+                console.log('data '+square.image.data)
+                console.log('values '+square.image.data.values)
+                let contents = square.image.data.values.contents
+                console.log(contents)
+                let newContents = []
                 if (contents.length>0) {
-                    for (let unit of contents) {
+                    while (contents.length>0)
+                    {
+                        let unit = contents.pop()
                         let newRow = unit.row+unit.rowChange
                         if (newRow==boardRows)
                         {
                             this.human.hp = this.human.hp - unit.attack
                             unit.hp = unit.hp - this.human.attack
+                            if (unit.hp<=0)
+                            {
+                                // Silently drop dead units
+                                unit.container.setPosition(0,0)
+                                continue
+                            }
                         }
                         else
                         {
+                            console.log(unit)
                             unit.row = newRow
-                            unit.container.y += squareY
+                            unit.container.y += squareY*unit.rowChange
                             console.log(unit)
                         }
                         unit.update()
+                        newContents.push(unit)
                     }
+                square.image.data.values.contents = newContents
                 }
             }
         }
         this.human.hpText.setText('HP '+this.human.hp+' ATK '+this.human.attack+' SHD '+this.human.shield)
     }
-    addUnits() 
+    addUnit(typ, c, r, rowChange)
+    {
+        this.unitIndex++
+        const unit = new Unit(this, this.unitIndex, typ, c, r, rowChange)
+        console.log(c+' '+r+' '+rowChange)
+        let data = this.mapData[r][c]
+        console.log(data)
+        data.image.data.values.contents.push(unit)
+}
+    addStartingUnits() 
     {
         this.unitData = []
         let level = levels[0]
         let units_def = level.players["computer"].units
-        let i = 0
         for (let def of units_def)
         {
-            console.log(def)
-            const unit = new Unit(this, i, 'unit_'+def.typ, def.c, def.r, 1)
-            this.mapData[def.c][def.r].contents.push(unit)
-            i++
+            this.addUnit(def.typ, def.c, def.r, 1)
         } 
-    }
-
-    createItems()
-    {   
-        let i = 0;
-        this.itemData = []
-        for (let c = 0; c<numItems; c++)
-        {
-            this.itemData[c] = new Item(this, i, 'unit_soldier',c,0)
-            i++
-        }
     }
 }
 
-class Example extends Phaser.Scene
+class Play extends Phaser.Scene
 {
     preload ()
     {
@@ -277,21 +336,58 @@ class Example extends Phaser.Scene
 
         timeText = this.add.text(100,0,'time: ', { fontSize: '14px'})
         this.stateText = this.add.text(100,20,'uninitialized', { fontSize: '14px'})
-        this.board = new Board(this, boardRows, boardColumns,0)
+        board = new Board(this, boardRows, boardColumns,0)
+        items = new Items(this, 0)
+
         this.stateText.setText('waiting_for_actions')
 
         const chargeButtonImg = this.add.image(0,0,"button_charge")
-        this.chargeButton = this.add.container(250,760)
-        this.chargeButton.setSize(80,80)
-        this.chargeButton.setInteractive()
-        this.chargeButton.add([chargeButtonImg])
-        this.chargeButton.on('pointerover', (pointer) => chargeButtonImg.setTint(0x808080))
-        this.chargeButton.on('pointerout', (pointer) => chargeButtonImg.clearTint())
-        this.chargeButton.on('pointerup', (pointer) => this.board.updateMap())
+        const chargeButton = this.add.container(250,760)
+        chargeButton.setSize(80,80)
+        chargeButton.setInteractive()
+        chargeButton.add([chargeButtonImg])
+        chargeButton.on('pointerover', (pointer) => chargeButtonImg.setTint(0x808080))
+        chargeButton.on('pointerout', (pointer) => chargeButtonImg.clearTint())
+        chargeButton.on('pointerup', (pointer) => board.updateMap())
+
+        this.physics.add.overlap(board.group, items.group, function(square, item)
+        {
+            //console.log('item/board collision')
+            //console.log(item)
+            let row = square.getData('row')
+            let column = square.getData('column')
+            item.setData('newSquare',square)
+            //console.log(row+' '+column)
+            //console.log(item)
+        })
 
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
             gameObject.x = dragX;
             gameObject.y = dragY;
+        })
+        this.input.on('dragend', function (pointer, gameObject) {
+            console.log('Here '+gameObject)
+            console.log(gameObject)
+            let values = gameObject.data.values
+            let newSquare = values.newSquare
+            console.log(typeof newSquare)
+            if (typeof newSquare != "undefined")
+            {
+                items.remove(gameObject)
+                let item = gameObject.getData('obj')
+                console.log('item')
+                console.log(item)
+                console.log('newSquare')
+                console.log(newSquare)
+                board.addUnit(item.typ, newSquare.getData('column'), newSquare.getData('row'), -1)
+                gameObject.destroy()
+                // TODO: 
+                // * create a corresponging unit
+                // * add the unit to the square
+                // * destroy item
+                //newSquare.data.values.contents.push(gameObject)
+                //gameObject.setData('newSquare',undefined)
+            }
         })
 
         this.timedEvent = this.time.addEvent({ delay: 2000, callback: this.onTimer, callbackScope: this, loop: true });
@@ -336,7 +432,14 @@ const config = {
     height: 900,
     backgroundColor: '#4a6741',
     parent: 'phaser-example',
-    scene: Example
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //gravity: { y: 300 },
+            debug: true
+        }
+    },
+    scene: Play
 };
 
 const game = new Phaser.Game(config);
